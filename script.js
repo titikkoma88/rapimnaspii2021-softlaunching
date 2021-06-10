a8ad9f5a5c4431c2705a242ba1687a31af7a6a28
titikkoma88 <titikkoma88@gmail.com>
2021-06-10 10:58:10 +0700

    Update Content

---
+++ script.js
@@ -0,0 +1,25 @@
+var countDate = new Date("Aug 20 2021 00:00:00").getTime();
+
+function newEvent() {
+  var now = new Date().getTime();
+  gap = countDate - now;
+
+  var detik = 1000;
+  var menit = detik * 60;
+  var jam = menit * 60;
+  var hari = jam * 24;
+
+  var h = Math.floor(gap / hari);
+  var j = Math.floor((gap % hari) / jam);
+  var m = Math.floor((gap % jam) / menit);
+  var d = Math.floor((gap % menit) / detik);
+
+  document.getElementById("hari").innerText = h;
+  document.getElementById("jam").innerText = j;
+  document.getElementById("menit").innerText = m;
+  document.getElementById("detik").innerText = d;
+}
+
+setInterval(function () {
+  newEvent();
+}, 1000);
